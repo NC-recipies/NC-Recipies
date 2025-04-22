499bc80375007790efc66f192a86a189be284a2e
NicholasOlivier1337 <nicholasolivier2@gmail.com>
2025-04-22 01:45:28 +0000

    database fix

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI="postgresql://nc_recipes_user:1qFSPOQl1KfCIJyRhtXsVbGnp14QexA3@dpg-d026d7muk2gs73e5hff0-a/nc_recipes"
-SECRET_KEY="1qFSPOQl1KfCIJyRhtXsVbGnp14QexA3"
+SQLALCHEMY_DATABASE_URI="postgresql://nc_recipes_8hwj_user:HEHszSPkehdVH57zxxptDETPvI1CbUHn@dpg-d03f70ili9vc73fk1bhg-a.oregon-postgres.render.com/nc_recipes_8hwj"
+SECRET_KEY="HEHszSPkehdVH57zxxptDETPvI1CbUHn"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
